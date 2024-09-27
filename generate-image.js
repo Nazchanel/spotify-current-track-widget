70b1ad4e2a5643af58673ec527912cb7b4bd0d62
Eshan Iyer <iyereshan@gmail.com>
2024-09-27 02:15:43 -0400

    Automation to update image every two minutes

---
+++ generate-image.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Change this URL to your live SVG endpoint
+  await page.goto('https://spotify-current-track-widget.onrender.com/', { waitUntil: 'networkidle0' });
+
+  const svg = await page.$('svg');
+  await svg.screenshot({ path: 'songs-pictures/image.png', type: 'png' });
+
+  await browser.close();
+})();
